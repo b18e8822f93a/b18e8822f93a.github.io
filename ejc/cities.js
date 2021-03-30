37ab7d4db84068bc2833395ae125aaf9bb630128
daniel <daniel>
2021-03-30 13:58:28 +0100

    30/03/2021

---
+++ ejc/cities.js
@@ -0,0 +1,1 @@
+var theData = [{"text":"City","children":[{"id":4,"text":"london"}]},{"text":"Position","children":[{"id":5,"text":"data_scientist"},{"id":6,"text":"developer"}]},{"text":"Commodity","children":[{"id":7,"text":"power"}]},{"text":"Company","children":[{"id":8,"text":"limejump"}]}]
